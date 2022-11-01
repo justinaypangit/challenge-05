654e1375cc1360b3d0c5cc592769ce6b2590537d
justinaypangit <just10gv@gmail.com>
2022-10-31 19:22:52 -0700

    Updated JS

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// create the planner
+$("#currentDay").text(moment().format("MMMM Do, YYYY"));
+// show current day
+// show different timeblocks for each hour
+// when clicking on each block you can enter an event
+// add save button
+let text = $('.text');
+let save = $('.saveBtn');
+let hour = moment().hour();
+
+console.log(hour);
+// when clicking save it saves the page
+text.each(function () {
+    let timeBlocks = parseInt($(this).attr('id'));
+
+    if (timeBlocks === hour) {
+        $(this).addClass('now');
+        $(this).removeClass('future');
+        $(this).removeClass('past');
+    }
+}
+
+)
+// try refresh to check if working
